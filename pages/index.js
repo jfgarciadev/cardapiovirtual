5437d525ee398549a601f34839ca849d02b58809
jfgarciadev <jfsoftwaredev@gmail.com>
2021-04-04 23:42:15 -0300

    db connect

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,46 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { connectToDatabase } from "../util/mongodb";
 
-export default function Home() {
+
+export default function Home({produtos}) {
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.header}>
 
-    
+      </div>
+
+      {produtos.map(produto => (
+        <div key={produto._id}>
+          {produto.nome}
+        </div>
+      ))}
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+       
       </footer>
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const { db } = await connectToDatabase('mongodb+srv://jfsoftwaredev:Escalobaloba_1999@cluster0.2brja.mongodb.net/juan?retryWrites=true&w=majority', 'juan');
+  const produtos = await db
+    .collection("produtos")
+    .find({})
+    .limit(1000)
+    .toArray();
+
+  console.log(produtos)
+  return {
+    props: {
+      produtos: JSON.parse(JSON.stringify(produtos)),
+    },
+  };
 }
